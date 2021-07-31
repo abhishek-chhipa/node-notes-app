df67b36078ba1354e9cc8fe0029c0a322746bbd0
Abhishek Chhipa <achhipa008@gmail.com>
2021-07-31 12:18:40 +0530

    changed a imp comment

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const yargs = require('yargs');
-const chalk = require ('chalk');
-const notes = require('./notes.js');
-
+const yargs = require("yargs");
+const chalk = require("chalk");
+const notes = require("./notes.js");
 
 // customize yagrs number
 // yargs.version('1.1.0');
@@ -9,71 +8,63 @@ const notes = require('./notes.js');
 // ad remove read list
 
 // create add command
-yargs
-    .command({
-        command:"add",
-        builder:{
-            title : {
-                describe:'Note Title',
-                demandOption:true,        // this means we definately habe to provide a title(required)
-                type:'string'
-            },
-            body :{
-                discribe:"Note Body",
-                demandOption:true,
-                type: 'string'
-            }
+yargs.command({
+  command: "add",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      discribe: "Note Body",
+      demandOption: true, // this means we definately habe to provide a title(required)
+      type: "string",
+    },
+  },
+  discribe: "add new note",
+  handler(argv) {
+    notes.addNotes(argv.title, argv.body);
+  },
+});
 
-        },
-        discribe:"add new note",
-        handler(argv){
-            notes.addNotes(argv.title,argv.body);
-        }
-})  
+yargs.command({
+  command: "remove",
+  builder: {
+    title: {
+      describe: "removing title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  discribe: "remove",
+  handler(argv) {
+    notes.removeNotes(argv.title);
+  },
+});
 
+yargs.command({
+  command: "list",
+  discribe: "listing a note",
+  handler() {
+    notes.listNotes();
+  },
+});
 
-yargs
-    .command({
-        command:'remove',
-        builder: {
-            title:{
-            describe:"removing title",
-            demandOption: true,
-            type: 'string'
-            }
-        },
-        discribe:"remove",
-        handler(argv) {
-            notes.removeNotes(argv.title);
-        }
-    })
-
-yargs
-    .command({
-        command:'list',
-        discribe:"listing a note",
-        handler() {
-            notes.listNotes();
-        }
-    })
-    
-    yargs
-    .command({
-        command:'read',
-        builder : {
-            title :{
-                describe : 'reading title',
-                demandOption: true,
-                type: 'string'
-            }
-        },
-        discribe:"read",
-        handler(argv) {
-            notes.readNotes(argv.title);
-        }
-    });
-
-
+yargs.command({
+  command: "read",
+  builder: {
+    title: {
+      describe: "reading title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  discribe: "read",
+  handler(argv) {
+    notes.readNotes(argv.title);
+  },
+});
 
 // console.log(yargs.argv);
 yargs.parse();
